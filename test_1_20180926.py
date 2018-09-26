2922dd6c5859a53ab06c428978290be090d84528
user <rainersbogdanovs@inbox.lv>
2018-09-26 09:55:57 +0300

    20180926_09_55

---
+++ test_1_20180926.py
@@ -0,0 +1,17 @@
+#mans_mainiigais = input("Cienījamais lietotāj, lūdzu ievadiet skaitli:")
+
+mans_mainiigais = raw_input("Cienījamais lietotāj, lūdzu ievadiet tekstu:")
+#x = mans_mainiigais * mans_mainiigais
+
+print("Lietotāj, Tu esi ievadījis tekstu: %s"%(mans_mainiigais))
+#print("Kā arī tagad atmiņā ir arī x = %s"%(x))
+
+# http://www.cplusplus.com/reference/cstdio/printf/
+'''
+print("Lietotāj, Tu esi ievadījis vērtību: %d"%(mans_mainiigais))
+print("Kā arī tagad atmiņā ir arī x = %d"%(x))
+'''
+'''
+print("Lietotāj, Tu esi ievadījis vērtību: %10.4f"%(mans_mainiigais))
+print("Kā arī tagad atmiņā ir arī x = %15.3f"%(x))
+'''
