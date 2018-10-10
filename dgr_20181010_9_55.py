6266ca06b249d5f21335ce2318f5e33bb5f80fc1
user <rainersbogdanovs@inbox.lv>
2018-10-10 09:56:59 +0300

    20181010_09_57

---
+++ dgr_20181010_9_55.py
@@ -0,0 +1,157 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+Greater than 4
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+Greater than 4
+Smaller
+Finis
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_2.py ===============
+Smaller
+Finis
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+Greater than 4
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_2.py ===============
+Smaller
+Finis
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+Greater than 4
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 21, in <module>
+    if x >= 5 :
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 21, in <module>
+    if x >= 5 :
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 21, in <module>
+    if x >= 5 :
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 23, in <module>
+    if x < 6 : print('Less than 6')
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 21, in <module>
+    if x >= 5:
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 4, in <module>
+    if x == 5:
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+Greater than 4
+Smaller
+Finis
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Before 5
+Is 5
+Is Still 5
+Third 5
+Afterwards 5
+Before 6
+Afterwards 6
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+More than one
+Less than 100
+All done
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Bigger
+All done
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+small
+All done
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Medium
+All done
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Large
+All done
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Medium
+All done
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 95, in <module>
+    if x < 2:
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/test_20181010_1.py", line 95, in <module>
+    if x < 2:
+NameError: name 'x' is not defined
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Below
+>>> 
+=============== RESTART: /home/user/RTR105/test_20181010_1.py ===============
+Below 20
+>>> 
---
+++ test_20181010_1.py
@@ -0,0 +1,105 @@
+'''
+#x = 5
+
+if x == 5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+
+#x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+print('Finis')
+
+
+#x = 5
+if x == 5:
+   print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or Equals 5')
+if x < 6: print('Less than 6')
+if x <= 5:
+    print('Less than or Equals 5')
+if x != 6:
+   print('Not equal 6')
+
+#x = 5
+print('Before 5')
+if x == 5:
+    print('Is 5')
+    print('Is Still 5')
+    print('Third 5')
+print('Afterwards 5')
+print('Before 6')
+if x == 6:
+    print('Is 6')
+    print('Is Still 6')
+    print('Third 6')
+print('Afterwards 6')
+
+#x = 42
+if x > 1:
+    print('More than one')
+    if x < 100:
+        print('Less than 100')
+print('All done')
+
+#Two-way Decisions with else:
+#x = 4
+if x > 2:
+    print('Bigger')
+else:
+    print('Smaller')
+print('All done')
+
+#x = 0
+if x < 2:
+    print('small')
+elif x < 10:
+    print('Medium')
+else:
+    print('Large')
+print('All done')
+
+#x = 5
+if x < 2:
+    print('small')
+elif x < 10:
+    print('Medium')
+else:
+    print('Large')
+print('All done')
+
+#x = 20
+if x < 2:
+    print('small')
+elif x < 10:
+    print('Medium')
+else:
+    print('Large')
+print('All done')
+
+#No else
+#x = 5 
+if x < 2:
+    print('small')
+elif x < 10:
+    print('Medium')
+    
+print('All done')
+'''
+x = 5
+
+if x < 2:
+    print('Below 2')
+elif x < 20:
+    print('Below 20')
+elif x < 10:
+    print('Below 10')
+else:
+    print('Something else')
+
