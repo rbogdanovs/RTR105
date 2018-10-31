e677114ced601d07a875f15f92ed11ba154167ed
user <rainersbogdanovs@inbox.lv>
2018-10-31 12:06:10 +0200

    20181031_12_06

---
+++ dgr_20181031.py
@@ -0,0 +1,117 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver1.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver1.py", line 19, in <module>
+    print("a1 = %.2f S1 = %.2f"%(a1,S2))
+NameError: name 'S2' is not defined
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver1.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 1.5
+1.5
+>>> 1.5
+1.5
+>>> 1
+1
+>>> 2.89
+2.89
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver1.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) = 1.00
+a0 = 1.50 S0 = 1.50
+a1 = -0.56 S1 = 0.94
+a2 = 0.06 S2 = 1.00
+a3 = -0.00 S3 = 1.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver2.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) = 1.00
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver2.py", line 14, in <module>
+    print("a0 = %.2f S0 = %.2f"%(a0,S))
+NameError: name 'S' is not defined
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver2.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) = 1.00
+a0 = 1.50 S0 = 1.50
+a1 = -0.56 S1 = 0.94
+a2 = 0.03 S1 = 0.97
+a3 = -0.00 S3 = 0.96
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver2.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) = 1.00
+a0 = 1.50 S0 = 1.50
+a1 = -0.56 S1 = 0.94
+a2 = 0.06 S1 = 1.00
+a3 = -0.00 S3 = 1.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver2.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 2.89
+sin(2.89) = 0.25
+a0 = 2.89 S0 = 2.89
+a1 = -4.02 S1 = -1.13
+a2 = 1.68 S1 = 0.55
+a3 = -0.33 S3 = 0.21
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) =   1.00
+a0 =  -1.50 S0 =  -1.50
+a1 =   0.46 S1 =  -1.04
+a2 =  -0.04 S2 =  -1.08
+a3 =   0.00 S3 =  -1.08
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) =   1.00
+a0 =  -1.50 S0 =  -1.50
+a1 =   0.56 S1 =  -0.94
+a2 =  -0.06 S2 =  -1.00
+a3 =   0.00 S3 =  -1.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 2.89
+sin(2.89) =   0.25
+a0 =  -2.89 S0 =  -2.89
+a1 =   4.02 S1 =   1.13
+a2 =  -1.68 S2 =  -0.55
+a3 =   0.33 S3 =  -0.21
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 0.25
+sin(0.25) =   0.25
+a0 =   0.25 S0 =   0.25
+a1 =  -0.00 S1 =   0.25
+a2 =   0.00 S2 =   0.25
+a3 =  -0.00 S3 =   0.25
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 1.5
+sin(1.50) =   1.00
+a0 =   1.50 S0 =   1.50
+a1 =  -0.56 S1 =   0.94
+a2 =   0.06 S2 =   1.00
+a3 =  -0.00 S3 =   1.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzu ievadi argumentu (x): 2.89
+sin(2.89) =   0.25
+a0 =   2.89 S0 =   2.89
+a1 =  -4.02 S1 =  -1.13
+a2 =   1.68 S2 =   0.55
+a3 =  -0.33 S3 =   0.21
+>>> 
