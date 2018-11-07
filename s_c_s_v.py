754e281d6249483d4af4176ca86d5ef33bd85c28
user <rainersbogdanovs@inbox.lv>
2018-11-07 09:28:36 +0200

    20181107_09_28

---
+++ s_c_s_v.py
@@ -0,0 +1,85 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: /home/user/RTR105/s_c_s_v4.py ===================
+Lietotāj, lūdzu ievadi skaitli ar2.89
+sin(2.89) =   0.25
+a0 =   2.89 S0 =   2.89
+a1 =  -4.02 S1 =  -1.13
+a2 =   1.68 S2 =   0.55
+a3 =  -0.33 S3 =   0.21
+>>> 
+=================== RESTART: /home/user/RTR105/s_c_s_v4.py ===================
+Lietotāj, lūdzu ievadi skaitli ar1.89
+sin(1.89) =   0.95
+a0 =   1.89 S0 =   1.89
+a1 =  -1.13 S1 =   0.76
+a2 =   0.20 S2 =   0.97
+a3 =  -0.02 S3 =   0.95
+>>> 
+=================== RESTART: /home/user/RTR105/s_c_s_v4.py ===================
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+sin(1.89) =   0.95
+a0 =   1.89 S0 =   1.89
+a1 =  -1.13 S1 =   0.76
+a2 =   0.20 S2 =   0.97
+a3 =  -0.02 S3 =   0.95
+>>> 
+========= RESTART: /home/user/RTR105/sin_caur_summu_ver4(orginal).py =========
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+sin(1.89) =   0.95
+a0 =   1.89 S0 =   1.89
+a1 =  -1.13 S1 =   0.76
+a2 =   0.20 S2 =   0.97
+a3 =  -0.02 S3 =   0.95
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver5.py =============
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+sin(1.89) =   0.95
+a0 =   1.89 S0 =   1.89
+a2 =  -0.34 S2 =   1.55
+a3 =   0.03 S3 =   1.58
+a4 =  -0.00 S4 =   1.58
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver5.py =============
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+sin(1.89) =   0.95
+a0 =   1.89 S0 =   1.89
+a1 =  -1.13 S1 =   0.76
+a2 =   0.20 S2 =   0.97
+a3 =  -0.02 S3 =   0.95
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu(last).py =============
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+standarta sin (1.89 =   0.95
+Izdruka no liet.f. a0 =   1.89 S0 =   1.89
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu(last).py", line 23, in <module>
+    yy = mans_sinuss(x)
+  File "/home/user/RTR105/sin_caur_summu(last).py", line 15, in mans_sinuss
+    print("Izdruka no liet.f. a%d = %6.2f S%d = %6.2f"%(a,S))
+TypeError: not enough arguments for format string
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu(last).py =============
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+standarta sin (1.89 =   0.95
+Izdruka no liet.f. a0 =   1.89 S0 =   1.89
+Izdruka no liet.f. a1 =  -1.13 S1 =  -2.13
+Izdruka no liet.f. a2 =   0.20 S2 =  -0.43
+Izdruka no liet.f. a3 =  -0.02 S3 =   0.01
+Izdruka no liet.f. a4 =   0.00 S4 =   0.00
+Izdruka no liet.f. Beigas!
+mans sin (1.89 =   0.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu(last).py =============
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 1.89
+standarta sin (1.89 =   0.95
+Izdruka no liet.f. a0 =   1.89 S0 =   1.89
+Izdruka no liet.f. a1 =  -1.13 S1 =   0.76
+Izdruka no liet.f. a2 =   0.20 S2 =   0.97
+Izdruka no liet.f. a3 =  -0.02 S3 =   0.95
+Izdruka no liet.f. a4 =   0.00 S4 =   0.95
+Izdruka no liet.f. Beigas!
+mans sin (1.89 =   0.95
+>>> 
