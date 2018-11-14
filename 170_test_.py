b111e8cdac959eccd00aef10ea5ae4dc475a6668
user <rainersbogdanovs@inbox.lv>
2018-11-14 14:36:54 +0200

    20181114_14_36

--- 170_test_.py
+++ 170_test_.py
@@ -1,8 +1,8 @@
 
-# Fails:170.py
-# Autors:...................
-# Apliecibasnumurs:.................
-# Datums:........
+# Fails: 170.py
+# Autors: Rainers Bogdanovs
+# Apliecibas numurs: 181Reb380
+# Datums: 2018.11.14
 # Sagatave funkcijas saknes mekleeshanai ar dihatomijas metodi
 
 #-*- coding: utf-8 -*-
@@ -25,11 +25,10 @@ if( funa * funb > 0.0 ):
     print("Dotajaa intervaalaa [%s,%s] saknju nav"%(a,b))
     sleep(1); exit() #Zinjo uz ekraana, gaida 1 sec. un darbu pabeidz
 else:
-    print("Dotaja aintervaalaa sakne(s) ir!")
+    print("Dotaja intervaalaa sakne(s) ir!")
     
 # Defineejam precizitaati, ar kaadu mekleesim sakni:
 deltax = 0.01
-
 # Sashaurinam saknes mekleeshanas robezhas:
 
 while ( fabs(b-a) > deltax ):
@@ -39,5 +38,10 @@ while ( fabs(b-a) > deltax ):
         b = x
     else:
         a = x
+        
+print("Sakne ir: ", x)
+y = sin(x)
+print("Saknes krustpunkts ir: ", y)
+c = (funb+funa)/2
 
-print("Sakne ir : ", x)
+print(c)
